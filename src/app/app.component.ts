8a75f590820d7ff23c8736f7e9755fa35c41e30e
Deepak <deepakaji2000@gmail.com>
2025-05-17 17:22:06 +0530

    Iniial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-name';
+  tableData:any;
+  constructor (
+    private httPClient : HttpClient
+  ){
+
+  }
+
+  ngOnInit(){
+    this.getTableDetails();
+  }
+
+  getTableDetails(){
+    this.httPClient.get<any[]>('https://zil-test.s3.us-east-1.amazonaws.com/json-data.json').subscribe(response =>{
+      console.log(response);
+      this.tableData = response;
+    },
+  )
+  }
 }
